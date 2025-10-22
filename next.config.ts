9aa9cdbc85daf5a38926b43bf0de23d863776f7b
gemadhc <gemadhc@gmail.com>
2025-10-22 15:36:58 -0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+ env:{
+    SERVER: "https://abps-erp.com/field",
+    QUICKBOOKS: "https://abps-erp/quickbooks"
+  }
+  eslint: {
+        ignoreDuringBuilds: true,
+    },
+  typescript: {
+        ignoreBuildErrors: true,
+    },
+
 };
 
 export default nextConfig;
