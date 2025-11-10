5275958323fe3c7c85b4cffd4dffd7a033972246
gemadhc <39023501+gemadhc@users.noreply.github.com>
2025-11-10 10:30:36 -0800

    added correction for json response

--- actions/session.js
+++ actions/session.js
@@ -1,6 +1,6 @@
 const server = process.env.SERVER;
 
-const requestAddresses = (id) => 
+const request = (id) => 
 	fetch(`${server}/session/addresses` + new URLSearchParams({ id }), {
     	method: "GET",
     	credentials: "include"
@@ -12,7 +12,6 @@ export const requestAddresses = async() =>{
 		let response = await requestAddresses(); 
 		let data = await response.json()
 		return JSON.parse(data.list)
-		
 	}catch(err){
 		return err; 
 	}
--- app/more/page.tsx
+++ app/more/page.tsx
@@ -1,8 +1,16 @@
 'use client'
 import AddressSelection from "../../components/AddressSelection"
 import {useState, useEffect} from 'react'
+import {requestAddresses} from "../../actions/session.js"
 
 export default function Home() {
+
+  useEffect(()=>{
+  	requestAddresses().then((data, err) =>{
+  		console.log("data: ", data)
+  	})
+
+  }, [])
   return (
     <div className="min-h-screen bg-white text-black ">
     	<div className = "h-100 bg-gray-100"> 
