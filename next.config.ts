2ba40035a6eaff7e4c46c5886631a6659bef38bf
gemadhc <gemadhc@gmail.com>
2025-11-12 16:19:30 -0800

    added key

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,8 @@ OFFICE: "https://abps-erp.com/backflow",
 TOKEN_SERVER : "https://api.intuit.com/quickbooks/v4/payments/tokens",
 VISA_METHOD_REF: "9", /* 9 for production and 3 for sandbox*/
 CASH_METHOD_REF: "4", 
-CHECK_METHOD_REF: "5"
+CHECK_METHOD_REF: "5", 
+GOOGLE_API_KEY: "AIzaSyDDN477JYDDS-g_c3hLBxR3HEnqaGKSfFo"
 },
 eslint: {
 ignoreDuringBuilds: true,
