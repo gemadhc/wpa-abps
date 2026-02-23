3c1b1e1312d9f6ce7a9886401e11eb3178af6b68
gemadhc <39023501+gemadhc@users.noreply.github.com>
2026-02-23 14:35:39 -0800

    added a better color for cancel button

--- components/StopCard.tsx
+++ components/StopCard.tsx
@@ -231,7 +231,7 @@ export default function StopCard({ stopID, item, reloadList}) {
             <div className="flex justify-end gap-3">
               <button
                 onClick={() => setOpenConfirmDialog(false)}
-                className="px-4 py-2 text-sm bg-gray-100 hover:bg-gray-200 rounded-lg"
+                className="px-4 py-2 text-sm text-gray-800 border border-gray-300 bg-gray-100 hover:bg-gray-200 rounded-lg"
               >
                 Cancel
               </button>
